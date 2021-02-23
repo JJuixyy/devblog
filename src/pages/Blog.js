3612c7d1dcd7db9a9617491b3701f61b22d17550
Anthony <ringeisen.anthony.dev@gmail.com>
2021-02-23 16:15:23 +0100

    .

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -3,7 +3,9 @@ import React, { useState } from 'react';
 
 const Blog = () => {
 
-    function handleChange(e){
+    const handleSubmit = () => {
+        setSubmit(true);
+        setPost({title,content,tags});
 
     }
 
@@ -14,24 +16,7 @@ const Blog = () => {
 
     return (
         <div>
-            Blog
-        <form className="post-form">
             
-            <label htmlFor="post-title">Titre du post :</label>
-            <input type="text" name="post-title" id="post-title" value={title} onChange={(e) =>setTitle(e.target.value)/>
-
-            <label htmlFor="post-content">Contenu : </label>
-            <textarea name="post-content" id="post-content" cols="30" rows="10" value={content} onChange={(e)=>setContent(e.target.value)}></textarea>
-
-            <label htmlFor="post-title">Titre du post :</label>
-
-            <input type="text" name="post_tags" id="post_tags" value={tags} onChange={(e) => {
-                let newTags = e.target.value.split(",");
-                setTags(newTags);
-            }}/>
-
-            <input type="submit" value="Créer mon post"/>
-        </form>
         </div>
     )
 }
